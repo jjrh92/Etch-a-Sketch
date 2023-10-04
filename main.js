508f0ca9038fd5468ad426ac4df8a871a1231686
Julio Reyes <juliojesusreyes@gmail.com>
2023-10-04 20:05:16 -0300

    Finishing: Needs to be styled better

--- main.js
+++ main.js
@@ -1,112 +1,86 @@
-// Parent element
+function drawBoard (size) {
 
-let body = document.getElementById ("body");
+    let board = document.querySelector (".board");
+    let squares = board.querySelectorAll ("div");
+    squares.forEach (div=> {div.remove ()});
 
-// Header
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-let header = document.createElement ("header");
-header.setAttribute ("class", "header");
-let headerLink = document.createElement ("a");
-headerLink.innerText = "Etch-a-Sketch";
-headerLink.setAttribute ("class","headerLink");
-headerLink.setAttribute ("title","Reload this page");
-headerLink.setAttribute ("draggable","false");
-headerLink.setAttribute ("href", "/");
-header.appendChild (headerLink);
-body.appendChild (header);
-
-// Main
-
-let main = document.createElement ("main");
-main.setAttribute ("class", "main");
-
-let drawContainer = document.createElement ("div");
-drawContainer.setAttribute ("class","drawContainer");
-drawContainer.setAttribute ("id","drawContainer");
-
-main.appendChild (drawContainer);
-body.appendChild (main);
-
-function createGrid (val) {
-
-    let squareSize = 16; // This defines the size of the square in pixels
-
-    val = squareSize;
-
-    drawContainer.style.display = "grid";
-    drawContainer.style.gridTemplateColumns = `repeat(${val}, 1fr)`
-    drawContainer.style.gridTemplateRows = `repeat(${val}, 1fr)`
-    drawContainer.style.height = squareSize*2;
-    drawContainer.style.width = squareSize*2;
-
-    for (let index = 1; index <= val*val; index++) {
+    let ammount = size * size;
+    for (let i = 0; i < ammount; i++) {
 
         let square = document.createElement ("div");
-        square.setAttribute ("class","square");
-        square.style.height = squareSize+"px";
-        square.style.width = squareSize+"px";
-        square.style.border = "1px solid red";
+        square.setAttribute ("class", "square");
+        square.addEventListener ("mouseover", colorSquare)
+        square.style.backgroundColor = "whitesmoke";
+        board.insertAdjacentElement ("beforeend", square);
 
-        square.addEventListener("mouseover", function handleMouse () {
-
-
-            square.style.backgroundColor=colorInput.value;
-
-        })
-
-
-        drawContainer.appendChild (square);
-        
     }
 
-    let colorContainer = document.createElement ("div");
-    colorContainer.setAttribute ("class", "colorContainer");
-    let colorInput = document.createElement ("input");
-    colorInput.setAttribute ("class", "colorInput");
-    let colorLabel = document.createElement ("label");
-    colorLabel.setAttribute ("for", "colorInput")
-    colorLabel.setAttribute ("class", "colorLabel");
-    colorLabel.innerText = "Pick a color"
-    let startingColor = "#002AFF";
-    colorInput.value = startingColor;
-    colorInput.setAttribute ("id", "colorInput");
-    colorInput.setAttribute ("type", "color");
-    colorContainer.appendChild (colorInput);
-    colorContainer.appendChild (colorLabel);
-    body.appendChild (colorContainer);
+    let rangeLabel = document.querySelector ("#rangeLabel");
+    rangeLabel.innerHTML ="Current Size: " + size + "x" + size;
 
-    // Size Input
-    let sizeContainer = document.createElement ("div");
-    sizeContainer.setAttribute ("class", "colorContainer");
-    let sizeInput = document.createElement ("input");
-    sizeInput.setAttribute ("class", "sizeInput");
-    let sizeLabel = document.createElement ("label");
-    sizeLabel.setAttribute ("for", "sizeInput")
-    sizeLabel.setAttribute ("class", "sizeLabel");
-    sizeLabel.innerText = "Canvas Size"
+    function resetSquare () {
 
-    sizeInput.setAttribute ("id", "sizeInput");
-    sizeInput.setAttribute ("type", "range");
-    sizeInput.setAttribute ("step", "2")
 
-    sizeInput.setAttribute ("min", "16")
-    sizeInput.setAttribute ("max", "22")
-
-    sizeInput.addEventListener("input", function setCurrentSize () {
-
-        let currentSize = sizeInput.value
-
-        sizeInput.setAttribute ("value", currentSize)
-        console.log (currentSize)
-        return currentSize
-
-    })
-
-    sizeInput.setAttribute ("title", sizeInput.value);
-    sizeContainer.appendChild (sizeInput);
-    sizeContainer.appendChild (sizeLabel);
-    body.appendChild (sizeContainer);
+        if (resetInput.checked === true) {
+    
+            let board = document.querySelector (".board");
+            let squares = board.querySelectorAll ("div");
+            squares.forEach (div=> {div.style.backgroundColor = "whitesmoke"});
+            window.location.reload ();
+            console.log ("♻️...")
+    
+        }
+    
+    }
+    let resetInput = document.querySelector (".resetInput");
+    resetInput.addEventListener ("click", resetSquare)
 
 }
 
-createGrid ()
+drawBoard (16)
+
+function changeSize (input) {
+
+    if (input >= 2 && input <= 100 ) {
+
+        drawBoard (input);
+
+    } else {
+
+        alert ("Invalid range value");
+        window.location.reload();
+
+    }
+
+}
+
+function colorSquare () {
+
+    let colorInput = document.querySelector (".colorInput");
+
+    if (eraserInput.checked === true) {
+
+        this.style.backgroundColor = "whitesmoke";
+        console.log ("🗑️...")
+
+    }
+
+    else if (rainbowInput.checked === true) {
+
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        console.log ("🌈...")
+
+    }
+
+    else {
+
+        this.style.backgroundColor = colorInput.value;
+        console.log ("🎨...")
+
+
+    }
+
+};
